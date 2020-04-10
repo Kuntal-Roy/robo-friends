9f0c0752bb10454fa651e4dccfa90f67a6536b40
Kuntal-Roy <roy.kuntal.98@gmail.com>
2020-04-10 21:57:49 +0530

    Updates

---
+++ precache-manifest.862872bf1764a2629d046a6d1c9fd6e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a34d3f4810410fa5e037813d30427f",
+    "url": "/robo-friends/index.html"
+  },
+  {
+    "revision": "c3ec2c53faf65940d3cf",
+    "url": "/robo-friends/static/css/main.222e0227.chunk.css"
+  },
+  {
+    "revision": "a3c1038f18e2e2385982",
+    "url": "/robo-friends/static/js/2.22aa8614.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robo-friends/static/js/2.22aa8614.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3ec2c53faf65940d3cf",
+    "url": "/robo-friends/static/js/main.124495a3.chunk.js"
+  },
+  {
+    "revision": "b3a85d9a170d1670d4c0",
+    "url": "/robo-friends/static/js/runtime-main.97cd4538.js"
+  }
+]);
